aed05643b34e5ac929a908b0174c12d34503967b
Keerti G <keerti.s.g2020@gmail.com>
2025-07-27 19:54:40 +0530

    Update routes.py

--- kpa_api/app/routes.py
+++ kpa_api/app/routes.py
@@ -4,7 +4,7 @@ from app import crud, schemas, database
 
 router = APIRouter()
 
-# -------- Wheel Spec Routes --------
+
 @router.post("/api/forms/wheel-specifications", response_model=schemas.WheelSpecResponse, status_code=201)
 def create_wheel_spec(spec: schemas.WheelSpecCreate, db: Session = Depends(database.get_db)):
     return crud.create_wheel_spec(db, spec)
@@ -13,8 +13,6 @@ def create_wheel_spec(spec: schemas.WheelSpecCreate, db: Session = Depends(datab
 def get_wheel_specs(formNumber: str = Query(None), db: Session = Depends(database.get_db)):
     return crud.get_wheel_specs(db, formNumber)
 
-
-# -------- Bogie Checksheet Routes --------
 @router.post("/api/forms/bogie-checksheet", response_model=schemas.BogieChecksheetResponse, status_code=201)
 def create_bogie_checksheet(data: schemas.BogieChecksheetCreate, db: Session = Depends(database.get_db)):
     return crud.create_bogie_checksheet(db, data)
